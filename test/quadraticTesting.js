f6c48a1db37d7aaf96ed8c000f5b59d1e8f15563
DAC-PhilThomsen <phil.thomsen@thedac.info>
2021-10-05 21:02:23 +0100

    added functionality for compatibility with Snapshot and extended test

--- test/quadraticTesting.js
+++ test/quadraticTesting.js
@@ -7,98 +7,109 @@ const ERC20 = artifacts.require('mockERC20');
 const duration = 10; // in days
 const startIn = 1;   // in days
 
-contract('vest', ([alice, bob, ben, minter]) => {
-  beforeEach(async () => {
-    this.MGH = await ERC20.new('MetaGameHub', 'MGH', 1000000, { from: minter });
-    this.V = await vest.new(this.MGH.address, duration, startIn);
-    await this.MGH.transfer(alice, 800000, { from: minter });
-    await this.MGH.transfer(bob, 200000, { from: minter });
-    await this.MGH.approve(await this.V.address, MAX_UINT256, { from: bob });
+contract('vest', ([alice, owner]) => {
+  before(async () => {
+    this.MGH = await ERC20.new('MetaGameHub', 'MGH', 2100000, { from: owner });
+    this.V = await vest.new(this.MGH.address, duration, startIn, { from: owner });
+    await this.MGH.transfer(alice, 1000000, { from: owner });
     await this.MGH.approve(await this.V.address, MAX_UINT256, { from: alice });
+    await this.MGH.approve(await this.V.address, MAX_UINT256, { from: owner });
     console.log('current Time: ' + (await time.latest()).toString());
     console.log('START TIME: ' + (await this.V.startTime.call()).toString());
   });
 
-  describe('deposit', () => {
-
-    it('should be possible to deposit before and after startTime for any account', async () => {
-      const start = await this.V.startTime.call();
-      await this.V.depositFor(bob, 200000, { from: bob });
-      await this.V.depositFor(alice, 600000, { from: alice });
-      await this.V.depositFor(ben, 100000, { from: alice });
-      assert.equal(await this.MGH.balanceOf(await this.V.address), 900000);
-      assert.equal(await this.V.getTotalBalance(ben), 100000);
-      assert.equal(await this.V.getTotalBalance(bob), 200000);
-      assert.equal(await this.V.getTotalBalance(alice), 600000);
-    
-      assert.equal(await this.V.getRetrievableAmount(bob), 0);
-      time.increaseTo(new BN(start));
-      await this.V.depositFor(bob, 100000, { from: alice });
-      await expectRevert(
-        this.V.retrieve({ from: bob }),
-        'nothing to retrieve',
-      );
-      assert.equal(await this.MGH.balanceOf(this.V.address), 1000000);
-    });
-  });
-
-
-  it('deposit for beneficiary and retrieve before start', async () => {
-    await this.MGH.approve(await this.V.address, MAX_UINT256, { from: bob });
-    //cannot retrieve anything before start of vesting:
-    await this.V.depositFor(ben, 200000, { from: bob });
-    assert.equal(await this.MGH.balanceOf(await this.V.address), 200000);
-    assert.equal(await this.V.getRetrievableAmount(bob), 0);
-    assert.equal(await this.V.getRetrievableAmount(ben), 0);
-    await expectRevert(
-      this.V.retrieve({ from: bob }),
-      'nothing to retrieve',
-    );
-    await expectRevert(
-       this.V.retrieve({ from: ben }),
-       'nothing to retrieve',
-    );
-    assert.equal(await this.MGH.balanceOf(this.V.address), 200000);
-    assert.equal(await this.V.getTotalBalance(ben), 200000);
-    assert.equal(await this.V.getTotalBalance(bob), 0);
-    assert.equal(await this.V.getRetrievablePercentage(), 0);
-  });
-
-  it('deposit for self and benificiary before and after start and withdraw at 1/7 and 1/2 and 1 and 2 of the duration', async () => {
+  it('depositAllFor and depositFor work and retrieve gives correct amount back', async () => {
     const start = await this.V.startTime.call();
-    await this.MGH.approve(await this.V.address, MAX_UINT256, { from: alice });
-    await this.MGH.approve(await this.V.address, MAX_UINT256, { from: bob });
-    await this.V.depositFor(alice, 700000, { from: alice });
-    await this.V.depositFor(bob, 100000, { from: bob });
-    await this.V.depositFor(ben, 100000, { from: bob });
-    assert.equal(await this.MGH.balanceOf(await this.V.address), 900000);
-    await time.increaseTo(await start.add( new BN(duration * 86400 / 7)));
-    let currently = (await time.latest()).toString();
-    console.log("85: " + currently);
+    await this.V.depositFor(alice, 600000, { from: alice });
+    await this.V.depositFor(alice, 100000, { from: owner });
+    await this.V.depositFor(owner, 500000, { from: owner });
+    assert.equal(await this.MGH.balanceOf(await this.V.address), 1200000);
+    assert.equal(await this.V.getTotalDeposit(owner), 500000);
+    assert.equal(await this.V.getTotalDeposit(alice), 700000);
 
-    await this.V.retrieve({ from: ben });
-    assert.equal((await this.V.getRetrievablePercentage()).toString(), '2');
-    assert.equal((await this.MGH.balanceOf(ben)).toString(), '2040');
-    await time.increaseTo(await start.add( new BN(duration * 86400 / 2)));
-    assert.equal((await this.V.getRetrievablePercentage()).toString(), '25');
-    await this.V.retrieve( { from: ben });
-    assert.equal((await this.MGH.balanceOf(ben)).toString(), '25000');
-    await time.increaseTo(await start.add( new BN (duration * 86400)));
-    await this.V.retrieve( { from: ben });
-    await this.V.retrieve( { from: alice });
-    assert.equal((await this.MGH.balanceOf(ben)).toString(), '100000');
-    assert.equal((await this.MGH.balanceOf(alice)).toString(), '800000');
-    await time.increaseTo(await start.add( new BN( 2 * duration * 86400)));
-    await this.V.retrieve({ from: bob });
-    await expectRevert(
-      this.V.retrieve({ from: ben }),
-      'nothing to retrieve',
-    );
+    assert.equal(await this.V.getRetrievableAmount({ from: alice }), 0);
+    assert.equal(await this.V.getRetrievablePercentage(), 0);
+
+    await time.increaseTo(start.add(new BN(10)));
+    await this.V.depositAllFor(alice, { from: owner });
+    assert.equal(await this.V.getTotalDeposit(alice), 1100000);
+    assert.equal(await this.MGH.balanceOf(owner), 100000);
+
     await expectRevert(
       this.V.retrieve({ from: alice }),
       'nothing to retrieve',
     );
-    assert.equal((await this.MGH.balanceOf(this.V.address)).toString(), '0');
-    assert.equal((await this.MGH.balanceOf(bob)).toString(), '100000');
+    await expectRevert(
+      this.V.retrieve({ from: owner }),
+      'nothing to retrieve',
+    );
+
+    await time.increaseTo(await start.add( new BN(duration * 86400 / 7)));
+    await this.V.retrieve({ from: alice });
+
+    await expectRevert(
+      this.V.retrieve({ from: alice }),
+      "nothing to retrieve",
+    );
+    assert.equal(await this.V.getRetrievablePercentage(), 2);
+    assert.equal((await this.MGH.balanceOf(alice)).toString(), "422440");
+
+    await this.V.depositFor(alice, 100000, { from: owner });
+    assert.equal(await this.V.getTotalDeposit(alice), 1200000);
+
+    await this.V.retrieve({ from: alice });
+    assert.equal((await this.MGH.balanceOf(alice)).toString(), "424480");
+    await expectRevert(
+      this.V.retrieve({ from: alice }),
+      "nothing to retrieve",
+    );
+    // INCREASE TIME TO HALF THE DURATION; TEST ADMIN FUNCTION
+    time.increaseTo(await start.add( new BN(duration * 86400 / 2)));
+    await expectRevert(
+      this.V.decreaseVesting(alice, 600000, { from: alice }),
+      "Ownable: caller is not the owner",
+    );
+    await expectRevert(
+      this.V.decreaseVesting(alice, 1200001, { from: owner }),
+      "revert",
+    );
+    await expectRevert(
+      this.V.decreaseVesting(alice, 1200000, { from: owner }),
+      "deposit has to be >= drainedAmount",
+    );
+    await this.V.decreaseVesting(alice, 600000, { from: owner });
+
+    assert.equal(await this.V.getRetrievableAmount({ from: alice}), 125520);
+
+    time.increaseTo(await start.add( new BN(duration * 86400 * 9 / 10)));
+    await this.V.retrieve({ from: owner });
+
+    await expectRevert(
+      this.V.retrieve({ from: owner }),
+      "nothing to retrieve",
+    );
+    assert.equal((await this.MGH.balanceOf(owner)).toString(), "405000");
+
+    await this.V.retrieve({ from: alice });
+    assert.equal((await this.MGH.balanceOf(alice)).toString(), "886000");
+
+    time.increaseTo(await start.add( new BN(duration * 86400)));
+
+    await this.V.retrieve({ from: alice });
+    await this.V.retrieve({ from: owner });
+
+    assert.equal((await this.MGH.balanceOf(alice)).toString(), "1000000");
+    assert.equal((await this.MGH.balanceOf(owner)).toString(), "500000");
+    assert.equal((await this.MGH.balanceOf(await this.V.address)).toString(), "600000");
+
+    await this.V.depositFor(alice, 500000, { from: alice });
+
+    // TEST balanceOf funcitonality for Snapshot
+    assert.equal((await this.V.balanceOf(alice)).toString(), "1000000");
+
+    await this.V.retrieve({ from: alice });
+    assert.equal((await this.MGH.balanceOf(alice)).toString(), "1000000");
+
+    console.log("HOORAY");
   });
 });
