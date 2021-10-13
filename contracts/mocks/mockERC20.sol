fb48c83c5a2d5d3411135b75cb8e4d68adfc2bd2
DAC-PhilThomsen <phil.thomsen@thedac.info>
2021-10-13 12:47:01 +0100

    move mockups in contracts for compilation

--- contracts/mocks/mockERC20.sol
+++ contracts/mocks/mockERC20.sol
@@ -8,6 +8,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract mockERC20 is ERC20 {
 
     constructor(string memory name, string memory symbol, uint256 supply) ERC20(name, symbol) {
-        _mint(msg.sender, supply);
+        _mint(msg.sender, supply*10**18);
     }
 }
