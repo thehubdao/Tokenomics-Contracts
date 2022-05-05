620aff94f783927534634ee4f7dc0c8ee6afa07f
DAC-PhilThomsen <phil.thomsen@thedac.info>
2022-05-05 03:30:48 +0100

    update util lib

--- contracts/roles/LibRoles.sol
+++ contracts/roles/LibRoles.sol
@@ -1,28 +1,23 @@
 pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-import "./AccessControlNFT.sol";
+import "./IAccessControlNFT.sol";
 
 library LibRoles {
 
-    function isTierEnabled(AccessControlNFT.RoleData storage role, AccessControlNFT.Tier tier) internal view returns(bool) {
+    function isTierEnabled(IAccessControlNFT.RoleData storage role, IAccessControlNFT.Tier tier) internal view returns(bool) {
         return role.feeByTier[tier] > 0;
     }
-
-    function hasRole(AccessControlNFT.User storage user, uint8 roleId) internal view returns(bool) {
-        return roleIndex(user, roleId) != type(uint256).max;
-    }
-
-    function roleIndex(AccessControlNFT.User storage user, uint8 roleId) internal view returns(uint256) {
+    
+    function roleIndex(IAccessControlNFT.User storage user, uint8 roleId) internal view returns(uint256) {
         uint256 index = user.indexByRole[roleId];
+        if(user.userRoleDataArray.length == 0) return type(uint256).max;
+        if(user.userRoleDataArray[0].roleId == roleId) return 0;
         return index == 0 ? type(uint256).max : index;
     }
 
-    function bestTierOf(AccessControlNFT.User storage user, uint8 roleId) internal view returns(AccessControlNFT.Tier) {
+    function bestTierOf(IAccessControlNFT.User storage user, uint8 roleId) internal view returns(IAccessControlNFT.Tier) {
         return user.userRoleDataArray[user.indexByRole[roleId]].tier;
     }
 
-    function addRole(AccessControlNFT.User storage user, AccessControlNFT.UserRoleData memory roleData) internal {
-        user.userRoleDataArray.push(roleData);
-    }
 }
