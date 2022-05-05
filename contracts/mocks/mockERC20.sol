40fc9dace326c63dfe8e92556e330563d5df1554
DAC-PhilThomsen <phil.thomsen@thedac.info>
2022-05-05 02:33:52 +0100

    unify erc20 mocks

--- contracts/mocks/mockERC20.sol
+++ contracts/mocks/mockERC20.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.8.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
@@ -7,11 +7,17 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract mockERC20 is ERC20 {
 
+    uint8 immutable _decimals;
     function mint(uint256 amount) public {
         _mint(msg.sender, amount * 10**18);
     }
 
-    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
-        _mint(msg.sender, initialSupply);
+    constructor(string memory name, string memory symbol, uint8 decimals_) ERC20(name, symbol) {
+        _mint(msg.sender, 10 ** (decimals_ + 6));
+        _decimals = decimals_;
+    }
+
+    function decimals() public view override returns(uint8) {
+        return _decimals;
     }
 }
