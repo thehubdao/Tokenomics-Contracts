3a25fb60c04fd7b08a5f20b3d3bbbba9cedbddff
DAC-PhilThomsen <phil.thomsen@thedac.info>
2022-05-05 02:38:45 +0100

    include UNIPool for testing RVS

---
+++ contracts/roles/IUniswapV2PairGetReserves.sol
@@ -0,0 +1,6 @@
+pragma solidity ^0.8.0;
+
+
+interface IUniswapV2PairGetReserves {
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
+}
